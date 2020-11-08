d75e7998e63f791d4f89444777e6a754d802d2f5
cnvs_my <mynguyen@Macpro-my.local>
2020-11-08 11:18:20 +0700

    Update page

--- basicTemplate/head.php
+++ basicTemplate/head.php
@@ -11,6 +11,7 @@
 <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/common/favicon.ico">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/common/css/reset.css" type="text/css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/common/css/common.css" type="text/css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/common/css/header.css" type="text/css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/common/css/footer.css" type="text/css">
